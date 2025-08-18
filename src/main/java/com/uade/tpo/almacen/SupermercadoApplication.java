eccff751c6832c5528cce804bc999e86d37cbb78
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-08-18 17:43:04 -0300

    Add files via upload

---
+++ src/main/java/com/uade/tpo/almacen/SupermercadoApplication.java
@@ -0,0 +1,15 @@
+package com.uade.tpo.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling // Habilita las tareas programadas
+public class SupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadoApplication.class, args);
+	}
+
+}

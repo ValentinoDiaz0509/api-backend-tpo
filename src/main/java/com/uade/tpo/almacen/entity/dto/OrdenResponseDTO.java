5c7913b557679cf2f6bffb07d1b2baf1e4c58df7
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-08-24 13:37:49 -0300

    Update OrdenResponseDTO.java

--- src/main/java/com/uade/tpo/almacen/entity/dto/OrdenResponseDTO.java
+++ src/main/java/com/uade/tpo/almacen/entity/dto/OrdenResponseDTO.java
@@ -4,16 +4,13 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public record OrdenResponseDTO(
-        int ordenId,
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Argentina/Buenos_Aires")
-        LocalDateTime fechaCreacion,
+        Long id,
+        LocalDateTime fecha,      
         String estado,
         BigDecimal subtotal,
         BigDecimal descuentoTotal,
         BigDecimal total,
-        String direccion,               
+        String direccion,
         List<ItemOrdenDTO> items
 ) {}

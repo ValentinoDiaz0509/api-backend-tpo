8aab57fe780e20f2d95cdd26054e6bec53e52a9b
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-08-24 13:24:17 -0300

    Update CommonBeans.java

--- src/main/java/com/uade/tpo/almacen/config/CommonBeans.java
+++ src/main/java/com/uade/tpo/almacen/config/CommonBeans.java
@@ -1,1 +1,14 @@
+package com.uade.tpo.almacen.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class CommonBeans {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}

9540b41990db6e9ef55c4a1b002e380608d13a22
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-08-24 13:48:34 -0300

    Update SupermercadoApplication.java

--- src/main/java/com/uade/tpo/almacen/SupermercadoApplication.java
+++ src/main/java/com/uade/tpo/almacen/SupermercadoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // Habilita las tareas programadas
+@EnableScheduling 
 public class SupermercadoApplication {
 
 	public static void main(String[] args) {

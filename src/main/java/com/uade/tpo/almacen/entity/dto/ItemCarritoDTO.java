aee89d938d7f6b56df92f4cdbd0dcf409fe2354e
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 22:50:46 -0300

    Update ItemCarritoDTO.java

--- src/main/java/com/uade/tpo/almacen/entity/dto/ItemCarritoDTO.java
+++ src/main/java/com/uade/tpo/almacen/entity/dto/ItemCarritoDTO.java
@@ -3,7 +3,8 @@ package com.uade.tpo.almacen.entity.dto;
 import java.math.BigDecimal;
 
 public record ItemCarritoDTO(
-        int productoId,
+        Long id,            
+        Long productoId,
         String nombreProducto,
         int cantidad,
         BigDecimal precioUnitario,

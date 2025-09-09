1b2abeacc405eabcda5601dcfbcd1972363139d4
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 23:24:51 -0300

    Update DireccionRepository.java

--- src/main/java/com/uade/tpo/almacen/repository/DireccionRepository.java
+++ src/main/java/com/uade/tpo/almacen/repository/DireccionRepository.java
@@ -3,8 +3,9 @@ package com.uade.tpo.almacen.repository;
 import com.uade.tpo.almacen.entity.Direccion;
 import com.uade.tpo.almacen.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface DireccionRepository extends JpaRepository<Direccion, Integer> {
+public interface DireccionRepository extends JpaRepository<Direccion, Long> {
     List<Direccion> findByUsuario(Usuario usuario);
 }

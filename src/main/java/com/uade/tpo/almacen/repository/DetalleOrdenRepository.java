04032afec3f56358804a43502159109e50f0a5d5
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 23:24:39 -0300

    Update DetalleOrdenRepository.java

--- src/main/java/com/uade/tpo/almacen/repository/DetalleOrdenRepository.java
+++ src/main/java/com/uade/tpo/almacen/repository/DetalleOrdenRepository.java
@@ -3,4 +3,4 @@ package com.uade.tpo.almacen.repository;
 import com.uade.tpo.almacen.entity.DetalleOrden;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DetalleOrdenRepository extends JpaRepository<DetalleOrden, Integer> {}
+public interface DetalleOrdenRepository extends JpaRepository<DetalleOrden, Long> {}

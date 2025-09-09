2632c1921a0116c408153398a43052742ed11cd0
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 23:25:47 -0300

    Update UsuarioRepository.java

--- src/main/java/com/uade/tpo/almacen/repository/UsuarioRepository.java
+++ src/main/java/com/uade/tpo/almacen/repository/UsuarioRepository.java
@@ -1,18 +1,15 @@
 package com.uade.tpo.almacen.repository;
 
 import com.uade.tpo.almacen.entity.Usuario;
+import com.uade.tpo.almacen.entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
     Optional<Usuario> findByEmail(String email);
-
+    List<Usuario> findByRol(Rol rol);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-
-    List<Usuario> findByRol(String rol);
 }

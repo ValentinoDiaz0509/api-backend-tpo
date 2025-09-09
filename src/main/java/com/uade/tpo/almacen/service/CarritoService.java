8c2eb344e2c71c7809a3bea0fc51c3a1d6d12fd5
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 23:26:44 -0300

    Update CarritoService.java

--- src/main/java/com/uade/tpo/almacen/service/CarritoService.java
+++ src/main/java/com/uade/tpo/almacen/service/CarritoService.java
@@ -3,8 +3,8 @@ package com.uade.tpo.almacen.service;
 import com.uade.tpo.almacen.entity.Carrito;
 
 public interface CarritoService {
-    Carrito obtenerOCrearCarrito(int usuarioId);
-    Carrito agregarItem(int usuarioId, int productoId, int cantidad);
-    Carrito quitarItem(int usuarioId, int itemId);
-    void vaciarCarrito(int usuarioId);
+    Carrito obtenerOCrearCarrito(Long usuarioId);
+    Carrito agregarItem(Long usuarioId, Long productoId, int cantidad);
+    Carrito quitarItem(Long usuarioId, Long itemId);
+    void vaciarCarrito(Long usuarioId);
 }

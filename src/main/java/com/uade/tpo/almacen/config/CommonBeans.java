5bd4f45d224f6b722c358ee4d31aced12505c228
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 22:41:30 -0300

    Update CommonBeans.java

--- src/main/java/com/uade/tpo/almacen/config/CommonBeans.java
+++ src/main/java/com/uade/tpo/almacen/config/CommonBeans.java
@@ -1,29 +1,8 @@
 package com.uade.tpo.almacen.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class CommonBeans {
 
-  @Bean
-  public OpenAPI api() {
-    final String schemeName = "bearerAuth";
-    return new OpenAPI()
-        .components(new Components().addSecuritySchemes(
-            schemeName,
-            new SecurityScheme()
-                .name(schemeName)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-        ))
-        // Seguridad global: todos los endpoints requieren bearer salvo los que dejaste en tu whitelist
-        .addSecurityItem(new SecurityRequirement().addList(schemeName));
-  }
 }
-

ffc224b7516329c491e674efa07e17670c634a6b
ValentinoDiaz0509 <166324234+ValentinoDiaz0509@users.noreply.github.com>
2025-09-08 22:50:20 -0300

    Update CarritoResponse.java

--- src/main/java/com/uade/tpo/almacen/entity/dto/CarritoResponse.java
+++ src/main/java/com/uade/tpo/almacen/entity/dto/CarritoResponse.java
@@ -1,14 +1,14 @@
 package com.uade.tpo.almacen.entity.dto;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CarritoResponse(
-    @JsonProperty("id") int id,
+    @JsonProperty("id") Long id,
     @JsonProperty("estado") String estado,
     @JsonProperty("items") List<ItemCarritoDTO> items,
-    @JsonProperty("total") double total
+    @JsonProperty("total") BigDecimal total
 ) implements Serializable {}
-
